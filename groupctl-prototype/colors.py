6f5085f8aee3ec539c9288dc389a47357ee9a724
Santiago de Leon <sdeleon28@gmail.com>
2026-05-23 21:56:34 -0300

    groupctl prototype: define bw color palette

---
+++ groupctl-prototype/colors.py
@@ -0,0 +1,33 @@
+from enum import StrEnum
+
+
+class BwColor(StrEnum):
+    _0_0_black = "_0_0_black"
+    _0_1_gray = "_0_1_gray"
+    _0_2_white = "_0_2_white"
+    _0_3_stone = "_0_3_stone"
+    _0_4_brown = "_0_4_brown"
+    _0_5_light_brown = "_0_5_light_brown"
+    _0_6_blue = "_0_6_blue"
+    _0_7_pale_blue = "_0_7_pale_blue"
+    _0_8_purple = "_0_8_purple"
+
+    _1_0_magenta = "_1_0_magenta"
+    _1_1_red = "_1_1_red"
+    _1_2_orange = "_1_2_orange"
+    _1_3_light_orange = "_1_3_light_orange"
+    _1_4_dark_lime = "_1_4_dark_lime"
+    _1_5_green = "_1_5_green"
+    _1_6_dark_aqua = "_1_6_dark_aqua"
+    _1_7_dark_cyan = "_1_7_dark_cyan"
+    _1_8_light_purple = "_1_8_light_purple"
+
+    _2_0_pink = "_2_0_pink"
+    _2_1_pink_red = "_2_1_pink_red"
+    _2_2_pale_orange = "_2_2_pale_orange"
+    _2_3_yellow = "_2_3_yellow"
+    _2_4_lime = "_2_4_lime"
+    _2_5_aqua = "_2_5_aqua"
+    _2_6_sky_blue = "_2_6_sky_blue"
+    _2_7_cyan = "_2_7_cyan"
+    _2_8_lightest_purple = "_2_8_lightest_purple"
